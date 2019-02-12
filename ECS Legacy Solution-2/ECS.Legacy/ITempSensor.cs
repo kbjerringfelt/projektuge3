fe624d5de4ee2dc49374af6577c0918fc0c51480
kbjerringfelt <karen.bjerringfelt@gmail.com>
2019-02-12 13:52:24 +0100

    Ændret kode til at passe med visualdiagram

---
+++ ECS Legacy Solution-2/ECS.Legacy/ITempSensor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS.Legacy
+{
+    public interface ITempSensor
+    {
+        // private Random gen = new Random();
+
+         int GetTemp();
+
+         bool RunSelfTest();
+    }
+}
