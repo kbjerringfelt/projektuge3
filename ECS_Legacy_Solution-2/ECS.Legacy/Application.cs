c7408382f6a421a1299c01bc2a66d714219db55c
Kajene <au594051@post.au.dk>
2019-02-13 11:17:21 +0100

    Ændret filnavn

---
+++ ECS_Legacy_Solution-2/ECS.Legacy/Application.cs
@@ -0,0 +1,16 @@
+﻿namespace ECS.Legacy
+{
+    public class Application
+    {
+        public static void Main(string[] args)
+        {
+            var ecs = new ECS(28, new TempSensor(), new Heater());
+
+            ecs.Regulate();
+
+            ecs.SetThreshold(20);
+
+            ecs.Regulate();
+        }
+    }
+}
